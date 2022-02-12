c982be41c1dabb1fa830918b1fe79f0cd4781040
Chris Siannas <xrsia@otenet.gr>
2022-02-12 07:44:33 +0200

    Start channels

---
+++ channels.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	//make a channel
+
+	//start a go function and through it send into the channel
+
+	//receive from the channel
+
+	//print the contents of the channel
+
+}
