4a6ebd39e75254733730b986da9a989d1211e3ce
Chris Siannas <xrsia@otenet.gr>
2022-02-12 07:49:19 +0200

    Finish

--- channels.go
+++ channels.go
@@ -1,12 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	//make a channel
+	ch := make(chan int)
 
 	//start a go function and through it send into the channel
-
+	go func(x, y int) {
+		result := x + y
+		ch <- result
+	}(12, 3)
 	//receive from the channel
-
+	number := <-ch
 	//print the contents of the channel
+	fmt.Printf("The number received from channel is: %d\n", number)
 
 }
